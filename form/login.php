a2f2e4bb297a98ba73460910560a7054922fa88b
wonsc12 <turnapple123@naver.com>
2024-02-06 13:13:38 +0900

    login php

---
+++ form/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>login</h1>
+    <form action="request.php">
+        <p>
+            <label for="name">Name: </label>
+            <input type="text" name="name" id="name">    
+        </p>
+        <p>
+            <label for="email">Email: </label>
+            <input type="email" name="email" id="email">    
+        </p>
+        <p>
+            <input type="submit" value="로그인">
+        </p>
+    </form>
+</body>
+</html>
